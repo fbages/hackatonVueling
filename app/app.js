e6e0d874f41c65f8a290ebf36bd82674aeaff147
unknown <francescbages@gmail.com>
2022-07-21 13:19:31 +0200

    app creat

--- app/app.js
+++ app/app.js
@@ -1,1 +1,11 @@
-console.log('Hello World');
+const express = require('express');
+const http = require('http')
+const app = express();
+const server = http.createServer(app);
+
+
+
+const PORT = 3000;
+server.listen(PORT, () => {
+    console.log(`Server is listening in port ${PORT}`)
+})
