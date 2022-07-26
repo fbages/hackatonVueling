f4a2a46561ee1b1bf6fdd5bcfa2a100d85eb01f8
Andrea Trussardi <andrea.trussardi@gmail.com>
2022-07-26 22:19:53 +0200

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -8,8 +8,8 @@ let db = {};
 
   async function initialize() {
     //const { host, port, databaseName } = config.dbmongo;
-//    await mongoose.connect("mongodb://" + "localhost" + ":" + "27017" + "/" + "hackatonPrevi_FBages");
-    await mongoose.connect("mongodb+srv://AndreTruss:bpbUGW0U6Gxl8kOE@cluster0.lnihe.mongodb.net/hackaton2?retryWrites=true&w=majority");
+    await mongoose.connect("mongodb://" + "localhost" + ":" + "27017" + "/" + "hackatonPrevi_FBages");
+    
     console.log("Conected to MongoDB database");
 
     db.products = mongoose.model("products", productSchema);
