cdf6b83a8b39b9739d4c169d1c9226c20f3bbfd2
AndreTruss <andrea.trussardi@gmail.com>
2022-07-26 12:25:19 +0200

    config

--- src/config/config.js
+++ src/config/config.js
@@ -8,7 +8,8 @@ let db = {};
 
   async function initialize() {
     //const { host, port, databaseName } = config.dbmongo;
-    await mongoose.connect("mongodb://" + "localhost" + ":" + "27017" + "/" + "hackatonPrevi_FBages");
+//    await mongoose.connect("mongodb://" + "localhost" + ":" + "27017" + "/" + "hackatonPrevi_FBages");
+    await mongoose.connect("mongodb+srv://AndreTruss:bpbUGW0U6Gxl8kOE@cluster0.lnihe.mongodb.net/hackaton?retryWrites=true&w=majority");
     console.log("Conected to MongoDB database");
 
     db.products = mongoose.model("products", productSchema);
